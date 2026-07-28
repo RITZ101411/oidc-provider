9abeb9145f462e162e187a7d383d3811283cdb30
RITZ_ <ritz1014.ritz1014@gmail.com>
2026-07-28 09:31:40 +0900

    chore: manage all env vars through .env and docker-compose

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { z } from '@hono/zod-openapi';
 
 const envSchema = z.object({
   PORT: z.string().optional().default('3000'),
-  ISSUER_URL: z.string().url().optional().default('http://localhost:3000'),
+  ISSUER_URL: z.string().url(),
   DATABASE_URL: z.string(),
 });
 
